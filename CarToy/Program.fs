53ead6161a88b94b9c53f1074d3905eb365a05f4
Chukwuma Akunyili <chukwuma.akunyili.233279@unn.edu.ng>
2023-12-30 17:00:26 +0100

    Add ToyCar and CarToy projects
    Implemented two new projects named ToyCar and CarToy, and configured their .csproj and .fsproj files respectively. Both projects contain core functionality codes written in C# and F#. Moreover, added .gitignore files at the root level and project level directories to ignore unneeded files specific to the IDE and OS from being tracked by Git.

---
+++ CarToy/Program.fs
@@ -0,0 +1,18 @@
+﻿open System
+
+[<EntryPoint>]
+
+let main _ =
+    Console.ReadLine()
+    |> int
+    |> fun numberLength ->
+        Console.ReadLine()
+        |> Seq.take numberLength
+        |> Seq.fold
+            (fun (speed, distance) button ->
+                ((if button = '+' then speed + 1 else max 0 (speed - 1)), distance + speed))
+            (0, 0)
+        |> snd
+        |> printfn "%d"
+
+    0
---
+++ ToyCar/Program.cs
@@ -0,0 +1,54 @@
+﻿// Declare namespace
+namespace ToyCar
+{
+    // Declare class
+    public static class Program
+    {
+
+        // Entry point of the program
+        private static void Main()
+        {
+            // Initialize variables
+            int number;
+            string buttons;
+
+            // Read the first line from console as integer. Catch errors.
+            try
+            {
+                number = int.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
+            }
+            catch
+            {
+                Console.WriteLine("Invalid input. Please enter an integer.");
+                return;
+            }
+
+            // Read the second line from console as string.
+            try
+            {
+                buttons = Console.ReadLine() ?? throw new InvalidOperationException();
+            }
+            catch
+            {
+                Console.WriteLine("Invalid input. Please enter a sequence of '+' and '-' characters.");
+                return;
+            }
+
+            // Initialize speed and distance
+            int speed = 0, distance = 0;
+
+            // Loop through characters in the string up to 'number' characters(length of the int numbers)
+            foreach (var btn in buttons.Take(number))
+            {
+                // If the character is '+', increase the speed. Otherwise decrease it (not below zero).
+                speed = btn == '+' ? speed + 1 : Math.Max(0, speed - 1);
+
+                // Add current speed to total distance
+                distance += speed;
+            }
+
+            // Output total distance
+            Console.WriteLine(distance);
+        }
+    }
+}
