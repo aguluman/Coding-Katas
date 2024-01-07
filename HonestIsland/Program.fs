6ce35c371383dbbf4d7f7463836fcc397d92a744
Chukwuma Akunyili <chukwuma.akunyili.233279@unn.edu.ng>
2024-01-07 14:34:17 +0100

    Add HonestIsland project with liar calculation logic
    This commit introduces the HonestIsland project that contains a console-based application written in F#. The added logic calculates possible numbers of liars based on an array of people's responses. The implementation involves sorting, mapping, and folding operations.

---
+++ HonestIsland/Program.fs
@@ -0,0 +1,30 @@
+﻿open System
+
+let getAllPossibleNumbersOfLiars (repliesUnsorted: int list) : int list =
+    let replies = List.sort repliesUnsorted
+
+    let possibleNumberOfLiars =
+        replies
+        |> List.mapi (fun i r -> (i, r))
+        |> List.fold
+            (fun acc (i, r) ->
+                if i <= r && (i = 0 || r <> List.item (i - 1) replies) then
+                    Set.add i acc
+                else
+                    acc)
+            Set.empty
+
+    if List.last replies < List.length repliesUnsorted then
+        Set.add (List.length repliesUnsorted) possibleNumberOfLiars
+    else
+        possibleNumberOfLiars
+    |> Set.toList
+    |> List.sort
+
+[<EntryPoint>]
+let main _ =
+    printfn "Enter the claims from each resident separated by space:"
+    let repliesUnsorted = Console.ReadLine().Split(' ') |> Array.map int |> Array.toList
+    let results = getAllPossibleNumbersOfLiars repliesUnsorted
+    printfn $"%A{results}"
+    0
